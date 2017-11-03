ee5811c80c85c520db30857b5b5c113914be6557
Dragan Djordjevic <draganit83@gmail.com>
2017-11-03 16:36:09 +0100

    My Project Setup Completed Successfully

---
+++ swhoppingbackend/src/main/java/net/kzn/swhoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kzn.swhoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
